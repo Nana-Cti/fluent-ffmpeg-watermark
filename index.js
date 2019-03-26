1d2146332387f64a61e5c9bf1344de1001aa7775
Nana-Cti <xiaohan026@163.com>
2019-03-26 17:54:35 +0800

    [添加批量操作]

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 var path = require('path');
 
 
-var vname = '6619626578831740000';
 var input = './input';
 var output = './output';
 var middle = './middle';
@@ -17,69 +16,82 @@ var middle = './middle';
 //     console.log('生成endingW视频成功');
 // }).save(path.join(__dirname, middle, `endingW.mp4`));
 
+fs.readdir(path.join(__dirname, input), (err, date) => {
+    !!err && console.log('读取文件夹错误:' + err.message);
+    var index = 0;
+    function handle() {
+        vname = path.basename(date[index], '.mp4');
+        new Promise((resolve) => {
 
-new Promise((resolve) => {
+            ffmpeg(path.join(__dirname, input, `${vname}.mp4`))
+            .ffprobe(0, function(err, metadata) {
+                !!err && console.log('无法读取视频大小:' + err.message);
+                var endName = metadata.streams[0].height > 600 ? 'endingH' : 'endingW';
+                var out = fs.createWriteStream(path.join(__dirname, middle, 'input.txt'), 'utf8');
+                out.write(`file '${path.join(__dirname, input, vname)}.mp4'\nfile '${path.join(__dirname, middle, endName)}.mp4'`)
+                out.end;
+                out.on('ready', function () {
+                    console.log('创建文本成功');
+                    resolve();
+                });
+            });
 
-    ffmpeg(path.join(__dirname, input, `${vname}.mp4`))
-    .ffprobe(0, function(err, metadata) {
-        !!err && console.log('无法读取视频大小:' + err.message);
-        var endName = metadata.streams[0].height > 600 ? 'endingH' : 'endingW';
-        var out = fs.createWriteStream(path.join(__dirname, middle, 'input.txt'), 'utf8');
-        out.write(`file '${path.join(__dirname, input, vname)}.mp4'\nfile '${path.join(__dirname, middle, endName)}.mp4'`)
-        out.end;
-        out.on('ready', function () {
-            console.log('创建文本成功');
-             resolve();
-        });
-    });
+        })
+        .then(() => {
 
-})
-.then(() => {
+            return new Promise((resolve) => {
+                ffmpeg(path.join(__dirname, middle,'input.txt'))
+                .inputOptions(
+                    '-f','concat',
+                    '-safe','0'
+                ).on('error', function (err) {
+                    console.log('合并视频发生错误: ' + err.message);
+                }).on('end', function () {
+                    console.log('合并视频成功');
+                    resolve();
+                }).save(path.join(__dirname, middle, 'first.mp4'));
+            });
+            
+        })
+        .then(() => {
 
-    return new Promise((resolve) => {
-        ffmpeg(path.join(__dirname, middle,'input.txt'))
-        .inputOptions(
-            '-f','concat',
-            '-safe','0'
-        ).on('error', function (err) {
-            console.log('合并视频发生错误: ' + err.message);
-        }).on('end', function () {
-            console.log('合并视频成功');
-            resolve();
-        }).save(path.join(__dirname, middle, 'first.mp4'));
-    });
-    
-})
-.then(() => {
+            return new Promise((resolve) => {
+                ffmpeg(path.join(__dirname, middle, 'first.mp4'))
+                .input(path.join(__dirname, middle, 'logo.png'))
+                .inputOptions(
+                    '-filter_complex','overlay=10:10'
+                ).on('error', function (err) {
+                    console.log('水印添加错误: ' + err.message);
+                }).on('end', function () {
+                    console.log('水印添加成功');
+                    resolve();
+                }).save(path.join(__dirname, middle, 'second.mp4'));
+            });
 
-    return new Promise((resolve) => {
-        ffmpeg(path.join(__dirname, middle, 'first.mp4'))
-        .input(path.join(__dirname, middle, 'logo.png'))
-        .inputOptions(
-            '-filter_complex','overlay=10:10'
-        ).on('error', function (err) {
-            console.log('水印添加错误: ' + err.message);
-        }).on('end', function () {
-            console.log('水印添加成功');
-            resolve();
-        }).save(path.join(__dirname, middle, 'second.mp4'));
-    });
+        })
+        .then(() => {
 
-})
-.then(() => {
+            return new Promise((resolve) => {
+                ffmpeg(path.join(__dirname, middle, 'second.mp4'))
+                .videoFilters("drawtext=fontfile=simhei.ttf:text='tttttTTT':x=100:y=10:fontsize=24:fontcolor=yellow:shadowy=2")
+                .on('error', function (err) {
+                    console.log('文字水印添加错误: ' + err.message);
+                }).on('end', function () {
+                    console.log('文字水印添加成功');
+                    resolve();
+                }).save(path.join(__dirname, output, `${vname}.mp4`));
+            });
 
-    return new Promise((resolve) => {
-        ffmpeg(path.join(__dirname, middle, 'second.mp4'))
-        .videoFilters("drawtext=fontfile=simhei.ttf:text='tttttTTT':x=100:y=10:fontsize=24:fontcolor=yellow:shadowy=2")
-        .on('error', function (err) {
-            console.log('文字水印添加错误: ' + err.message);
-        }).on('end', function () {
-            console.log('文字水印添加成功');
-            resolve();
-        }).save(path.join(__dirname, output, `${vname}.mp4`));
-    });
+        })
+        .then(() => {
+            console.log(path.join(__dirname, output, `${vname}.mp4`));
+            index++;
+            if (date.length<index) {
+                handle();
+            }
+        })
 
-})
-.then(() => {
-    console.log(path.join(__dirname, output, `${vname}.mp4`));
-})
+    };
+
+    handle();
+});
